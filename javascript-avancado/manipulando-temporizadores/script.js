c7daf711f8c4ff01b934a3068ebe3a06ea9fd682
Rendell Arruda <rendellarruda3@gmail.com>
2022-12-15 14:44:34 -0300

    setTimerOut and setInterval class

---
+++ javascript-avancado/manipulando-temporizadores/script.js
@@ -0,0 +1,24 @@
+/* setInterval executa de tempos em tempos
+// function action() {
+//   document.write('Executando...<br/>');
+// }
+
+// funcao e tempo em milisegundos
+// var timer = setInterval(() => {
+//   document.write('Executando...<br/>');
+// }, 1000);
+
+//dar clearInterval(funcao) no console para parar o setInterval
+*/
+
+//setTimeOut executa uma vez só
+// function action() {
+//   document.write('Executando...<br/>');
+// }
+
+// setTimeout(action, 3000);
+//ou
+
+setTimeout(() => {
+  document.write('Executando TimerOut...<br/>');
+}, 3000);
--- javascript-avancado/treinando-conhecimentos/script.js
+++ javascript-avancado/treinando-conhecimentos/script.js
@@ -5,4 +5,32 @@ let imc;
 function calcular(event) {
   event.preventDefault();
   peso = document.getElementById('peso').value;
+  altura = document.getElementById('altura').value;
+  imc = peso / (altura * altura);
+
+  resultado = document.querySelector('#res');
+
+  if (imc < 17) {
+    resultado.innerHTML = `Seu resultado foi ${imc.toFixed(
+      2
+    )}<br>Você esta muito abaixo do peso`;
+  } else if (imc < 18.9) {
+    resultado.innerHTML = `Seu resultado foi ${imc.toFixed(
+      2
+    )}<br>Você esta abaixo do peso`;
+  } else if (imc < 24.99) {
+    resultado.innerHTML = `Seu resultado foi ${imc.toFixed(
+      2
+    )}<br>Seu peso é normal`;
+  } else if (imc < 29.99) {
+    resultado.innerHTML = `Seu resultado foi ${imc.toFixed(
+      2
+    )}<br>Você esta acima do peso`;
+  } else {
+    resultado.innerHTML = `Seu resultado foi ${imc.toFixed(
+      2
+    )}<br>Você esta na obesidade`;
+  }
+  document.getElementById('peso').value = '';
+  document.getElementById('altura').value = '';
 }
