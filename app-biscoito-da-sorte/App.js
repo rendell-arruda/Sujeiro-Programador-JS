9e89bbb8f94a1c3bb424fb35db2dff62020eb7b4
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-02 18:21:53 -0300

    app biscoito da sorte parte 1

--- app-biscoito-da-sorte/App.js
+++ app-biscoito-da-sorte/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+function App() {
+  const [img, setImg] = useState(require('./assets/biscoito.png'));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={img} style={styles.img} />
+      <Text style={styles.textofrase}>
+        "esta é minha primeira frase do biscoito"
+      </Text>
+      <TouchableOpacity style={styles.botao} onPress={() => alert('')}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar biscoito</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]}
+        onPress={() => alert('')}
+      >
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, { color: '#121212' }]}>
+            Reiniciar biscoito
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +32,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  img: { width: 230, height: 230 },
+  textofrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
 });
+
+export default App;
