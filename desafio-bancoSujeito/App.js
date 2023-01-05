168fa93b2278a8c8b4124ea2ab7613046e01ad8a
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-05 20:33:53 -0300

    feat picker and slider

--- desafio-bancoSujeito/App.js
+++ desafio-bancoSujeito/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 import {
   StyleSheet,
   Text,
@@ -8,7 +9,7 @@ import {
   Image,
   SafeAreaView,
   TextInput,
-  TouchableOpacity
+  ScrollView
 } from 'react-native';
 
 import Button from './src/Button';
@@ -32,72 +33,93 @@ export default function App() {
 
   const [status, setStatus] = useState(false);
 
+  const [limite, setLimiteSelecionado] = useState('');
+
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView>
       {/* TITULO */}
-      <View style={styles.tituloArea}>
-        <Text style={styles.titulo}>Banco Sujeito</Text>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: 'https://w7.pngwing.com/pngs/859/407/png-transparent-bankgarantie-computer-icons-finance-bank-trademark-logo-bank.png'
+      <SafeAreaView style={styles.container}>
+        <View style={styles.tituloArea}>
+          <Text style={styles.titulo}>Banco Sujeito</Text>
+          <Image
+            style={styles.logo}
+            source={{
+              uri: 'https://w7.pngwing.com/pngs/859/407/png-transparent-bankgarantie-computer-icons-finance-bank-trademark-logo-bank.png'
+            }}
+          />
+        </View>
+
+        {/* IMPUT */}
+        <View style={styles.area}>
+          <Text style={styles.inputText}>Informe os seus dados</Text>
+          <TextInput
+            placeholder={'Digite seu nome'}
+            style={styles.input}
+            onChangeText={text => {
+              setNome(text);
+            }}
+          />
+
+          <TextInput
+            placeholder={'Digite sua idade'}
+            style={styles.input}
+            keyboardType="number-pad"
+            onChangeText={text => {
+              setIdade(text);
+            }}
+          />
+        </View>
+        {/*  */}
+
+        {/* PICKER */}
+        <View style={[styles.pickerArea, styles.area]}>
+          <Text style={styles.pickerText}>Selecione o seu Sexo</Text>
+          <Picker
+            selectedValue={sexoSelecionado}
+            onValueChange={(itemValue, itemIndex) =>
+              setSexoSelecionado(itemValue)
+            }
+          >
+            {sexoItem}
+          </Picker>
+        </View>
+        {/*  */}
+        {/* SLIDER */}
+        <View style={styles.area}>
+          <Text>Escolha o seu Limite</Text>
+          <Slider
+            minimumValue={200}
+            maximumValue={2000}
+            onValueChange={valorSelecionado => {
+              setLimiteSelecionado(valorSelecionado);
+            }}
+          />
+          <Text style={{ textAlign: 'center' }}>R$ {limite.toFixed(0)},00</Text>
+        </View>
+        {/*  */}
+
+        {/* SWITCH */}
+        <Switch
+          value={status}
+          onValueChange={valorSelecionado => {
+            setStatus(valorSelecionado);
           }}
+          trackColor={{ false: '#121212', true: '#00ff00' }}
+          thumbColor={status ? '#121212' : '#f4f4f4'}
         />
-      </View>
+        <Text style={{ textAlign: 'center', fontSize: 25 }}>
+          Status: {status ? 'ATIVO' : 'Inativo'}
+        </Text>
+        {/*  */}
 
-      {/* IMPUT */}
-      <View style={styles.inputArea}>
-        <TextInput
-          placeholder={'Digite seu nome'}
-          style={styles.input}
-          onChangeText={text => {
-            setNome(text);
-          }}
-        />
+        {/* BOTAO */}
+        <Button />
 
-        <TextInput
-          placeholder={'Digite sua idade'}
-          style={styles.input}
-          keyboardType="number-pad"
-          onChangeText={text => {
-            setIdade(text);
-          }}
-        />
-      </View>
-      {/*  */}
-
-      {/* PICKER */}
-      <Picker
-        selectedValue={sexoSelecionado}
-        onValueChange={(itemValue, itemIndex) => setSexoSelecionado(itemValue)}
-      >
-        {sexoItem}
-      </Picker>
-      {/*  */}
-      {/* SLIDER */}
-      {/*  */}
-
-      {/* SWITCH */}
-      <Switch
-        value={status}
-        onValueChange={valorSelecionado => {
-          setStatus(valorSelecionado);
-        }}
-        trackColor={{ false: '#121212', true: '#00ff00' }}
-        thumbColor={status ? '#121212' : '#f4f4f4'}
-      />
-      <Text style={{ textAlign: 'center', fontSize: 25 }}>
-        Status: {status ? 'ATIVO' : 'Inativo'}
-      </Text>
-      {/*  */}
-
-      {/* BOTAO */}
-      <Button />
-
-      {/* CLIENTE */}
-      <Cliente nome={nome} idade={idade} />
-      <Text>{sexo[sexoSelecionado].nome}</Text>
-    </SafeAreaView>
+        {/* CLIENTE */}
+        {/* <Cliente nome={nome} idade={idade} /> */}
+        {/* <Text>{sexo[sexoSelecionado].nome}</Text> */}
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
@@ -106,26 +128,45 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 35
   },
+  area: {
+    marginHorizontal: 10,
+    marginVertical: 20,
+    borderWidth: 1,
+    padding: 5
+  },
   tituloArea: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 20,
-    marginBottom: 20
+    marginTop: 20
   },
   titulo: {
     fontSize: 20
   },
+
   logo: {
     marginLeft: 10,
     width: 30,
     height: 30
   },
-  inputArea: {},
   input: {
     borderWidth: 1,
     padding: 10,
     marginHorizontal: 10,
     marginBottom: 20
+  },
+  inputText: { textAlign: 'center', marginBottom: 10 },
+  pickerArea: {
+    borderColor: '#121212',
+    borderWidth: 1
+    // marginHorizontal: 10,
+    // marginVertical: 10
+  },
+  pickerText: {
+    fontSize: 15,
+    textAlign: 'center',
+    backgroundColor: '#121212',
+    color: '#fff',
+    padding: 10
   }
 });
