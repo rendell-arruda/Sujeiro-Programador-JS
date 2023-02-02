103bd31c292ae3d4c594a2d4375c55365c3eaaf4
Rendell Arruda <rendellarruda3@gmail.com>
2023-02-02 10:01:20 -0300

    aula de eventos

--- reactJS-comecando/meusite/src/App.js
+++ reactJS-comecando/meusite/src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hora: '00:00:00'
-    };
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() });
-    }, 1000);
-  }
-
-  componentDidUpdate() {
-    console.log('atualizou');
-  }
-
   render() {
     return (
       <div>
         <h1>Meu projeto</h1>
-        {this.state.hora}
+        <Membro nome="Visitante" />
       </div>
     );
   }
