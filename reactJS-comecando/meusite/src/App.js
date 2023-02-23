1fb121a99e0cad39bd211f119af0cb0a878269e0
Rendell Arruda <rendellarruda3@gmail.com>
2023-02-23 20:26:00 -0300

    aula de props

--- reactJS-comecando/meusite/src/App.js
+++ reactJS-comecando/meusite/src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+
+const Bemvindo = props => {
+  return (
+    <div>
+      <h2>Bem vindo {props.nome}</h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  );
+};
 
 function App() {
-  return (<div><h1>Olá Mundo</h1></div>)
+  return (
+    <div>
+      <Bemvindo nome="Mateus" idade="29" />
+      <Bemvindo nome="Amanda" idade="27" />
+      <h1>Olá Mundo</h1>
+    </div>
+  );
 }
 
 export default App;
