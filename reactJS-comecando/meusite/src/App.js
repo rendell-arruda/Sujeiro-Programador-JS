7db86fce77b15abbc9751087cfcc5abc7cc1014d
Rendell Arruda <rendellarruda3@gmail.com>
2023-02-24 08:04:27 -0300

    ciclo de vida dos componentes

--- reactJS-comecando/meusite/src/App.js
+++ reactJS-comecando/meusite/src/App.js
@@ -1,43 +1,29 @@
 import React, { Component } from 'react';
-
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      nome: 'Mateus',
-      contador: 0
-    };
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
-  }
-
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-    state.nome = 'José';
-    this.setState(state);
-  }
-
-  diminuir() {
-    let state = this.state;
-    if (state.contador === 0) {
-      alert('Opa chegou a 0');
-      return;
+    {
+      super(props);
+      this.state = {
+        hora: '00:00:00'
+      };
     }
-    state.contador -= 1;
-    this.setState(state);
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log('Atualizou');
   }
 
   render() {
     return (
       <div>
-        <h1>Contador</h1>
-        {this.state.nome}
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
+        <h1>Meu projeto</h1>
+        <h3>{this.state.hora}</h3>
       </div>
     );
   }
