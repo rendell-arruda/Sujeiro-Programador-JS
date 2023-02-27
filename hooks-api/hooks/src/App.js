9821a76dc68bbf1e8871d9cb93fb513d2fc70fde
Rendell Arruda <rendellarruda3@gmail.com>
2023-02-27 10:55:08 -0300

    useeffect

--- hooks-api/hooks/src/App.js
+++ hooks-api/hooks/src/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  //declarar uma nova variável de state, na qual chamaremos de 'contador'
+  const [tarefas, setTarefas] = useState([]);
 
-  const [contador, seContador] = useState(0);
+  const [input, setInput] = useState('');
 
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  function handleAdd() {
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
   return (
     <div>
-      <h1>Contador</h1>
-      <p>You clicked {contador} times</p>
-      <button onClick={() => seContador(contador + 1)}>Aumentar</button>
+      <h1>Lista de Tarefas</h1>
+      <ul key={tarefas}>
+        {tarefas.map(tarefa => (
+          <li>{tarefa}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onChange={e => setInput(e.target.value)}
+        value={input}
+      />
+      <button onClick={handleAdd}>Cadastar tarefa</button>
     </div>
   );
 }
