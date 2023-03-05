dc50403eb11b175b9104b212b8e05e93e6cbef47
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-05 15:37:59 -0300

    add itens na lista e estiliza

---
+++ reactJS-avancado/prime-flix/src/pages/Favoritos/index.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
+  useEffect(() => {
+    const minhaLista = localStorage.getItem('@primeflix');
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
+  return (
+    <div className="meus-filmes">
+      <h1>Minha lista de Filmes Favoritos</h1>
+      <ul>
+        {filmes.map(item => {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button>Excluir</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Favoritos;
--- reactJS-avancado/prime-flix/src/routes.js
+++ reactJS-avancado/prime-flix/src/routes.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Filme from './pages/Filme';
 import Home from './pages/Home';
 import Erro from './pages/Erro';
+import Favoritos from './pages/Favoritos';
 
 function RoutesApp() {
   return (
@@ -12,6 +13,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Erro />} />
       </Routes>
