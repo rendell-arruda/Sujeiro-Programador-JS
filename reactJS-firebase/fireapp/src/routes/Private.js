468c84e5b27acdfda5496eb548269323012f2b41
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-24 19:13:24 -0300

    add private para verificar login

---
+++ reactJS-firebase/fireapp/src/routes/Private.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebaseConnection'
+import { Navigate } from 'react-router-dom'
+
+export default function Private({ children }) {
+  const [loading, setLoading] = useState(true)
+  const [signed, setSigned] = useState(false)
+
+  useEffect(() => {
+    async function checkLogin() {
+      const unsub = onAuthStateChanged(auth, user => {
+        if (user) {
+          // se tem usuario logado
+          const userData = {
+            uid: user.uid,
+            email: user.email
+          }
+
+          localStorage.setItem('@datailUser', JSON.stringify(userData))
+          setLoading(false)
+          setSigned(true)
+        } else {
+          // se nao tem usuario logado
+          setLoading(false)
+          setSigned(false)
+        }
+      })
+    }
+    checkLogin()
+  }, [])
+
+  if (loading) {
+    return <div></div>
+  }
+
+  if (!signed) {
+    return <Navigate to="/" />
+  }
+
+  return children
+}
--- reactJS-firebase/fireapp/src/routes/index.js
+++ reactJS-firebase/fireapp/src/routes/index.js
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Register from '../pages/Register';
 import Admin from '../pages/Admin';
+import Private from './Private';
 
 export default function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      <Route path='/admin' element={<Admin/>}/>
+
+      <Route path='/admin' element={ <Private><Admin/></Private>}/>
     </Routes>
   );
 }
