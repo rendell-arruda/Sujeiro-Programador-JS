3f9843d86780d6368bab3cbd0c40f98c56c88a6e
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-24 20:22:39 -0300

    add logica para registrar tarefa no db

--- reactJS-firebase/fireapp/src/pages/Admin/index.jsx
+++ reactJS-firebase/fireapp/src/pages/Admin/index.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import './admin.css';
-import { useState } from 'react';
-import { auth } from '../../firebaseConnection';
+import { useState, useEffect } from 'react';
+import { auth, db } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 
 export default function Admin() {
   const [tarefaInput, setTarefaInput] = useState('');
+  const [user, setUser] = useState({});
 
-  function handleRegister(e) {
+  useEffect(() => {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem('@detailUser');
+      setUser(JSON.parse(userDetail));
+    }
+    loadTarefas();
+  }, []);
+
+  async function handleRegister(e) {
     e.preventDefault();
+    if (tarefaInput === '') {
+      alert('Digite sua tarefa...');
+      return;
+    }
+    await addDoc(collection(db, 'tarefas'), {
+      tarefa: tarefaInput,
+      created: new Date(),
+      userUid: user?.uid
+    })
+      .then(() => {
+        console.log('Tarefa Registrada');
+        setTarefaInput('');
+      })
+      .catch(error => {
+        console.log('Erro ao registrar ' + error);
+      });
   }
 
   async function handleLogout() {
--- reactJS-firebase/fireapp/src/routes/Private.js
+++ reactJS-firebase/fireapp/src/routes/Private.js
@@ -18,7 +18,7 @@ export default function Private({ children }) {
             email: user.email
           };
 
-          localStorage.setItem('@datailUser', JSON.stringify(userData));
+          localStorage.setItem('@detailUser', JSON.stringify(userData));
           setLoading(false);
           setSigned(true);
         } else {
