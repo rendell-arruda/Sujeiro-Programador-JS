219d5fdc3dfefd58ca7cd6fb4b08278ac98dacef
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-01 11:52:18 -0300

    add dashboard e register pages

--- react-SistemaDeChamados/sistema-de-chamados/src/pages/SignUp/index.js
+++ react-SistemaDeChamados/sistema-de-chamados/src/pages/SignUp/index.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-    return (
-        <div>Cadasto</div>
-    )
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+        <form>
+          <h1>Cadastrar nova conta</h1>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/">Já possui uma conta? Faça Login</Link>
+      </div>
+    </div>
+  );
 }
--- react-SistemaDeChamados/sistema-de-chamados/src/routes/index.js
+++ react-SistemaDeChamados/sistema-de-chamados/src/routes/index.js
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
+import { Routes, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
-import React from 'react'
+import React from 'react';
+import Dashboard from '../pages/Dashboard';
 
 export default function RoutesApp() {
-    return (
-        <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/register" element={<SignUp />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
