237f7b95dceb1b2ba4941420ac9eb03300f312f8
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-01 11:21:53 -0300

    page login

--- react-SistemaDeChamados/sistema-de-chamados/src/pages/SignIn/index.js
+++ react-SistemaDeChamados/sistema-de-chamados/src/pages/SignIn/index.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import { useState } from 'react';
+import React from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 export default function SignIn() {
-    return (
-        <div>SignIn</div>
-    )
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma Conta</Link>
+      </div>
+    </div>
+  );
 }
